da4d8608a67422d053b0e4a2ca888f8ac22e8f0c
newDmoon <20125dimanovogrodsky@gmail.com>
2025-08-25 17:59:11 +0300

    feat: movie-service entities

--- movie-service/src/main/java/com/example/movie/Main.java
+++ movie-service/src/main/java/com/example/movie/Main.java
@@ -1,4 +1,4 @@
-package com.example.clicker;
+package com.example.movie;
 
 public class Main {
     public static void main(String[] args) {
--- movie-service/src/main/java/com/example/movie/entity/Movie.java
+++ movie-service/src/main/java/com/example/movie/entity/Movie.java
@@ -1,2 +1,20 @@
-package com.example.movie.entity;public class Movie {
+package com.example.movie.entity;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class Movie {
+    private UUID id;
+    private String title;
+    private String description;
+    private ArrayList<String> genres;
+    private short ageRestriction;
+    private String director;
+    private short durationInMinutes;
+    private LocalDateTime showFromDate;
+    private LocalDateTime showToDate;
+    private ArrayList<String> inRoles;
+
+    private ArrayList<UUID> showTimes;
 }
