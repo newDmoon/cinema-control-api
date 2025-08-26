639e6053b64cfa39c2776689afeb22b1fc0dc30f
newDmoon <20125dimanovogrodsky@gmail.com>
2025-08-26 14:15:54 +0300

    feat: spring boot deps: jpa, security, start auth chema

--- auth-service/src/main/java/com/example/auth/entity/User.java
+++ auth-service/src/main/java/com/example/auth/entity/User.java
@@ -1,10 +1,27 @@
 package com.example.auth.entity;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
 
+@Table(name = "users", schema = "auth")
+@Data
+@Entity
 public class User {
+    @Id
+    @GeneratedValue
+    @Column(columnDefinition = "UUID")
     private UUID id;
+
+    @Column(unique = true, nullable = false)
     private String name;
     private String email;
     private String passwordHash;
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @CollectionTable(name = "user_roles", schema = "auth")
+    private Set<String> roles = new HashSet<>();
 }
